8cc81c2bccc9d61cfeec08e8944137bd37231b46
Pavel Emelyanov <xemul@scylladb.com>
2021-03-30 12:02:06 +0300

    fix io-prof to collect full disk profile
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- io-prof/run.sh
+++ io-prof/run.sh
@@ -1,68 +1,35 @@
 #!/bin/bash
 
+set -e
+
 DUR=80
 PAUS=10
-MOD="rand"
-DIR="/home/xfs/io_tester"
+DIR="/mnt"
+IOT="../../seastar/build/dev/apps/io_tester/io_tester"
 
 function do_measure {
-    ./build/dev/apps/io_tester/io_tester --directory ${DIR} --conf conf.yaml -c1 --duration $DUR | awk 'BEGIN{all=""}/throughput:/{tot+=$2;all=all":"int($2/1000)}END{print tot/1000 all}'
+    $IOT --storage ${DIR} --conf conf.yaml -c1 --duration $DUR | awk '/IOPS/{res=res" "$2}END{print res}'
     sleep $PAUS
 }
 
-PRLS="1 2 4 8 16 32"
+PRLS="1 2 4 8 16 32 64 128 256"
 
-function measure_equal {
-    echo -n "${1}/${2}"
-    for prl in $PRLS; do
-        cat tmpl.yaml | sed -e 's/LD1/'${MOD}${1}'/' -e 's/LD2/'${MOD}${2}'/' -e 's/PRL1/'${prl}'/' -e 's/PRL2/'${prl}'/' > conf.yaml
-        res=$(do_measure)
-        echo -n " ${res}"
-    done
-    echo ""
-}
-
-function measure_shifted {
-    if [ "${1}" == "0" ]; then
-        echo -n "read-/write+"
-    else
-        echo -n "read+/write-"
-    fi
-
-    for prl in $PRLS; do
-        if [ "$prl" == "1" ]; then
-            echo -n " 0"
-            continue
-        fi
-
-        if [ "${1}" == "0" ]; then
-            prlr=$((prl/2))
-            prlw=$((prl + prl/2))
-        else
-            prlr=$((prl + prl/2))
-            prlw=$((prl/2))
-        fi
-        cat tmpl.yaml | sed -e 's/LD1/'${MOD}'read/' -e 's/LD2/'${MOD}'write/' -e 's/PRL1/'${prlr}'/' -e 's/PRL2/'${prlw}'/' > conf.yaml
-        res=$(do_measure)
-        echo -n " ${res}"
-    done
-    echo ""
-}
-
-# parallelizm 1 2 4 8 16 32 64 128
-# rr          . . . . . . . .
-# ww          . . . . . . . .
-# 50-50
-
-echo -n "parallelizm"
 for prl in $PRLS; do
-    echo -n "             ${prl}"
+	echo -n "read ${prl} "
+        cat tmpl-p.yaml | sed -e 's/LD/seqread/' -e 's/PRL/'${prl}'/' > conf.yaml
+	do_measure
 done
-echo ""
 
-#measure_shifted 1
-#measure_shifted 0
+for prl in $PRLS; do
+	echo -n "write ${prl} "
+        cat tmpl-p.yaml | sed -e 's/LD/seqwrite/' -e 's/PRL/'${prl}'/' > conf.yaml
+	do_measure
+done
 
-measure_equal "read" "read"
-measure_equal "read" "write"
-measure_equal "write" "write"
+for rprl in $PRLS; do
+	for wprl in $PRLS; do
+		echo -n "read ${rprl} write ${wprl} "
+		cat tmpl.yaml | sed -e 's/LD1/seqread/' -e 's/LD2/seqwrite/' -e 's/PRL1/'${rprl}'/' -e 's/PRL2/'${wprl}'/' > conf.yaml
+		do_measure
+	done
+done
