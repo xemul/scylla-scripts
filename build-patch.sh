f9dbaf884cf8166d50f74947a0669dc9c2d47216
Pavel Emelyanov <xemul@scylladb.com>
2024-02-29 15:54:39 +0300

    build headers as well
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- build-patch.sh
+++ build-patch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-FILES=$(git diff -p HEAD $1 | lsdiff -p1 | fgrep .cc | sed -e 's#^#build/dev/#' -e 's#\.cc$#.o#')
+FILES=$(git diff -p HEAD $1 | lsdiff -p1 | egrep '.cc|.hh' | sed -e 's#^#build/dev/#' -e 's#\.cc$#.o#' -e 's#\.hh#.hh.o#')
 echo ${FILES}
 ninja -j24 ${FILES}
